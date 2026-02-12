54e677cef54713be41984913c58ee1e1a41f2fef
Monalisa222 <monalisadengre222@gmail.com>
2026-02-12 12:52:20 +0530

    Add associations and validations for User, Organization, and Membership

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,2 +1,6 @@
 class Organization < ApplicationRecord
+  has_many :memberships, dependent: :destroy
+  has_many :users, through: :memberships
+
+  validates :name, presence: true, uniqueness: true
 end
