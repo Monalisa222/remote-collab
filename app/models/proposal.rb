a4fb29c7f5eb211163f8801aac9e9528bbf28522
Monalisa222 <40558158+Monalisa222@users.noreply.github.com>
2026-02-16 15:26:02 +0530

    Feature/live updates
    * Enable Turbo Stream broadcasting for proposals
    
    * Subscribe proposals index to Turbo Stream broadcasts

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -2,4 +2,6 @@ class Proposal < ApplicationRecord
   belongs_to :organization
 
   validates :title, presence: true
+
+  broadcasts_to ->(proposal) { "organization_#{proposal.organization_id}_proposals" }
 end
