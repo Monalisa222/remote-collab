e2b71b11327c61295e19ca9e9caed7265fcfd0f5
Monalisa222 <monalisadengre222@gmail.com>
2026-02-17 12:59:41 +0530

    Add Task model with status enum and proposal association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_16_065433) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_17_072736) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -42,6 +42,16 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_16_065433) do
     t.index ["title"], name: "index_proposals_on_title"
   end
 
+  create_table "tasks", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.bigint "proposal_id", null: false
+    t.integer "status", default: 0, null: false
+    t.string "title", null: false
+    t.datetime "updated_at", null: false
+    t.index ["proposal_id"], name: "index_tasks_on_proposal_id"
+    t.index ["status"], name: "index_tasks_on_status"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", null: false
@@ -54,4 +64,5 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_16_065433) do
   add_foreign_key "memberships", "organizations"
   add_foreign_key "memberships", "users"
   add_foreign_key "proposals", "organizations"
+  add_foreign_key "tasks", "proposals"
 end
