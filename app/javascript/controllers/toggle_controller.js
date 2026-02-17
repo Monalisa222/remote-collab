460030a09eec5d227dfa6e5c40a458a81f1157ad
Monalisa222 <monalisadengre222@gmail.com>
2026-02-17 12:19:34 +0530

    Implement toggle behavior using Stimulus

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,6 +2,9 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
-  connect() {
+  static targets = ["content"]
+
+  toggle() {
+    this.contentTarget.classList.toggle("hidden")
   }
 }
