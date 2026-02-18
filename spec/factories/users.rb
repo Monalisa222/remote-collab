3c58857c93fad31b7360fd1e79773d2da290fe43
Monalisa222 <monalisadengre222@gmail.com>
2026-02-18 14:11:40 +0530

    Fix password confirmation spelling

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     name { Faker::Name.name }
     email { Faker::Internet.unique.email }
     password { "password123" }
-    passoword_confirmation { "password123" }
+    password_confirmation { "password123" }
   end
 end
